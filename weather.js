536d49f8e40b17f7e497091e723094becf0dad4f
IvanLowLLH <ivanlowlh@gmail.com>
2025-11-20 14:18:39 +0800

    Update backend to add update UI

--- weather.js
+++ weather.js
@@ -6,15 +6,31 @@ async function getWeatherLocation(location) {
     const date1 = "2025-11-20"
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}/${date1}?unitGroup=metric&contentType=json&key=${API_KEY}`);
     const weatherData = await response.json();
-    const dateTemp = weatherData.days[0].temp;
-    console.log(`The temperature in ${location} for ${date1} is ${dateTemp} C`);
+    return weatherData;
 }
 
-form.addEventListener("submit", (event) => {
+function updateUI(weatherData) {
+    const todayWeatherUI = document.getElementById("today-weather");
+    todayWeatherUI.innerHTML = "";
+
+    const resolvedLocation = weatherData.resolvedAddress;
+    const dateTemp = weatherData.currentConditions.temp;
+    const weatherConditions = weatherData.currentConditions.conditions;
+
+    todayWeatherUI.innerHTML = `
+    <h3 class="resolved-location">${resolvedLocation}</h3>
+    <p class="weather-info">Temperature is now ${dateTemp} C. </p>
+    <p class="weather-info">Weather now: ${weatherConditions}. </p>
+    `
+}
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
     const formData = new FormData(form);
 
     const location = formData.get("location");
     console.log(location)
-    getWeatherLocation(location);
-    event.preventDefault();
+    const weatherData = await getWeatherLocation(location);
+    console.log(weatherData)
+    updateUI(weatherData);
 })
